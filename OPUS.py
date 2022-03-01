Fixed normalization and correct identifikation

OPUS.py
@@ -83,6 +83,9 @@ class OPUS_measurement(QThread):
         filename_pre = f"out_pre_measurement.txt"
         product = 'test'
 
+        reduced_path = path_file.strip('')
+        normalized_spectrum_path = f'{reduced_path}/1.Der.'
+        print(normalized_spectrum_path)
 
         compatible = self.opusrequest(host, port, "COMMAND_LINE MakeCompatible ([%s], [%s], {});" % (pfad_leermessung_spektrum, path_file))
         print("Compatible? {}".format(compatible[0]))
@@ -112,9 +115,9 @@ class OPUS_measurement(QThread):
                 measurement_data_raw.append(np.nan)
 
         derivative2 = self.opusrequest(host, port, "COMMAND_LINE Derivative ([%s], {QSP=13, QOD=1});" % path_file)
-        time.sleep(5)
-        normalization = self.opusrequest(host, port, "COMMAND_LINE  Normalize ([%s], {NME=2, NFX=4000.000000, NLX=400.000000, NWR=1});" % path_file)
-        time.sleep(5)
+        #time.sleep(5)
+        normalization = self.opusrequest(host, port, "COMMAND_LINE  Normalize ([%s], {NME=2, NFX=4000.000000, NLX=400.000000, NWR=1});" % normalized_spectrum_path)
+        #time.sleep(5)
         read2 = self.opusrequest(host, port, "READ_FROM_FILE %s;" % path_file)
         read3 = self.opusrequest(host, port, "READ_FROM_BLOCK AB/1.Der.")
         read4 = self.opusrequest(host, port, "READ_HEADER")
@@ -148,6 +151,7 @@ class OPUS_measurement(QThread):
         csvlist_raw.loc[len(csvlist_raw)] = measurement_data_raw
         unload = self.opusrequest(host, port, "UNLOAD_FILE %s" % str(path_file))
         unload = self.opusrequest(host, port, "UNLOAD_FILE %s" % str(path_leermessung))
+        unload = self.opusrequest(host, port, "UNLOAD_FILE %s" % str(normalized_spectrum_path))
         return csvlist_raw, csvlist_pre
         
     def load_csvfile(self,x_values):
@@ -183,9 +187,9 @@ class OPUS_measurement(QThread):
         csvlist_raw = self.load_csvfile(x_values_leer)
 
         # answer= self.opusrequest("127.0.0.1", 80, "COMMAND_LINE LoadFile('/mnt/c/Users/G164.PHYTAX/Desktop/phydent/Leermessung.0', WARNING)")
-        command = self.opusrequest("127.0.0.1", 80, "MeasureSample(0, {NSS=24, EXP='ATR_Di_Phydent.XPM', XPP='C:\\Users\\G164\\Desktop\\Phydent-App\\XPM\\ATR_Di_Phydent.XPM')")
+        command = self.opusrequest("127.0.0.1", 80, "MeasureSample(0, {NSS=4, EXP='ATR_Di_Phydent.XPM', XPP='C:\\Users\\G164\\Desktop\\Phydent-App\\XPM\\ATR_Di_Phydent.XPM')")
         #print(command)
-        time.sleep(30)
+        #time.sleep(30)
         #Obtain information about selected/just measured spectrum
         filenames= self.opusrequest("127.0.0.1", 80,"GET_SELECTED")
         print(filenames)